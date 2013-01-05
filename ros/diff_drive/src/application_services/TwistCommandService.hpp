37d8517cac6324bd4dbf9f900a97d15e096c5179
Sawyer Larkin <github@toborguru.net>
2013-01-05 15:37:21 -0700

    Removed p_impl implementation for the TwistCommandService.

--- ros/diff_drive/src/application_services/TwistCommandService.hpp
+++ ros/diff_drive/src/application_services/TwistCommandService.hpp
@@ -4,27 +4,39 @@
 #define GUARD_TwistCommandService
  
 #include <boost/shared_ptr.hpp>
+
+#include "diff_drive/TickVelocity.h"
+
+#include "BaseModel.hpp"
+#include "TwistConverter.hpp"
+#include "TwistConverter.hpp"
+#include "ITickVelocityListener.hpp"
 #include "ITickVelocityEndpoint.hpp"
 #include "ITwistEndpoint.hpp"
-#include "BaseModel.hpp"
  
 namespace diff_drive_application_services
 {
-  class TwistCommandService
-  {
-    public:
-      explicit TwistCommandService( boost::shared_ptr<diff_drive_core::ITickVelocityEndpoint> tick_velocity_endpoint,
-                                    boost::shared_ptr<diff_drive_core::ITwistEndpoint>        twist_endpoint,
-                                    boost::shared_ptr<const diff_drive_core::BaseModel>       base_model );
+class TwistCommandService : public diff_drive_core::ITickVelocityListener
+{
+public:
+  explicit TwistCommandService( boost::shared_ptr<diff_drive_core::ITickVelocityEndpoint> tick_velocity_endpoint,
+                                boost::shared_ptr<diff_drive_core::ITwistEndpoint>        twist_endpoint,
+                                boost::shared_ptr<const diff_drive_core::BaseModel>       base_model );
 
-      void BeginAcceptingCommands();
-      void StopAcceptingCommands();
- 
-    private:
-      // Forward declare the implementation class
-      class TwistCommandServiceImpl;
-      boost::shared_ptr<TwistCommandServiceImpl> _p_impl;
-  };
+  void OnTickVelocityAvailableEvent(const diff_drive::TickVelocity& tick_velocity);
+
+  void BeginAcceptingCommands();
+  void StopAcceptingCommands();
+
+private:
+  diff_drive_core::TwistConverter _twist_converter;
+
+  boost::shared_ptr<diff_drive_core::ITickVelocityEndpoint> _p_tick_velocity_endpoint;
+  boost::shared_ptr<diff_drive_core::ITwistEndpoint>        _p_twist_endpoint;
+  boost::shared_ptr<const diff_drive_core::BaseModel>       _p_base_model;
+
+  bool  _is_accepting_commands;
+};
 }
  
 #endif /* GUARD_TwistCommandService */
