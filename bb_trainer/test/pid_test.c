37cda4958cfac083035e4e0799b5a2654b8ba1f9
Sawyer <github@toborguru.net>
2020-04-04 12:24:03 -0600

    Added pid test failure conditions and fixed a bug in pid.c that was exceeding output limits.

--- bb_trainer/test/pid_test.c
+++ bb_trainer/test/pid_test.c
@@ -17,23 +17,41 @@
 
 TEST(PidTest, staticData )
 {
-  int16_t input = 0;
   int16_t output;
+  int16_t num_iterations = 250;
+  int16_t num_steps = 10;
+  int16_t starting_setpoint = 1;
+  int16_t ending_setpoint = 12;
+  int16_t target_setpoint = 6;
+  int16_t max_output = 255;
+  int16_t min_output = -255;
 
   Pid_State_t pid_state;
+
+  memset(&pid_state, 0, sizeof(pid_state));
 
   pid_state.kp = 5.0;
   pid_state.kd = 0.0;
   pid_state.ki = 1.0;
 
-  pid_state.setpoint = 6;
+  pid_state.max_output = max_output;
+  pid_state.min_output = min_output;
 
-  for ( int_fast8_t i = 0; i < 100; i++)
+  int16_t input = starting_setpoint;
+  int16_t step_size = (ending_setpoint - starting_setpoint) / num_steps;
+  int16_t step_length = num_iterations / num_steps;
+
+  pid_state.setpoint = target_setpoint;
+
+  for ( int i = 0; i < num_iterations; i++)
   {
     output = Pid_Compute_Output( input, &pid_state );
     printf( "Input: %d, output: %d\n", input, output );
 
-    input = output / 20;
+    input = starting_setpoint + ( step_size * (i / step_length) );
+
+    ASSERT_GE(output, min_output);
+    ASSERT_LE(output, max_output);
   }
 
   EXPECT_EQ( 1, 1 );
--- bb_trainer/util/pid.c
+++ bb_trainer/util/pid.c
@@ -22,21 +22,23 @@ int16_t Pid_Compute_Output( int16_t input, Pid_State_t *_p_state )
 
     // Compute the error derivative (using "Derivative on Measurement")
     _p_state->d_error = input - _p_state->prev_input;
+    _p_state->prev_input = input;
 
     // Compute error sum
     _p_state->i_term += _p_state->ki * (float)_p_state->error;
 
-    // Limit sum to max potential
+    // Limit integral sum to max potential
     if ( _p_state->i_term > _p_state->max_output) _p_state->i_term = _p_state->max_output;
     if ( _p_state->i_term < _p_state->min_output) _p_state->i_term = _p_state->min_output;
-
-    // Update the previous error
-    _p_state->prev_input = input;
 
     // Compute the output
     output = (int16_t)( (_p_state->kp * (float)_p_state->error)
                         - (_p_state->kd * (float)_p_state->d_error)
                         + _p_state->i_term );
+
+    // Limit output to max
+    if ( output > _p_state->max_output) output = _p_state->max_output;
+    if ( output < _p_state->min_output) output = _p_state->min_output;
 
     return output;
 }
